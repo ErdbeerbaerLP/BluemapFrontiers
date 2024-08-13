67d8769036703c81124f061153230ba3c971a426
ErdbeerbaerLP <admin@erdbeerbaerlp.de>
2024-08-13 14:13:46 +0200

    1.20.6 Port

--- quilt/src/main/java/de/erdbeerbaerlp/bm_mf/quilt/BluemapFrontiersQuilt.java
+++ quilt/src/main/java/de/erdbeerbaerlp/bm_mf/quilt/BluemapFrontiersQuilt.java
@@ -4,12 +4,10 @@ import de.erdbeerbaerlp.bm_mf.BluemapFrontiers;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
-import de.erdbeerbaerlp.bm_mf.fabriclike.BluemapFrontiersFabricLike;
-
 public final class BluemapFrontiersQuilt implements ModInitializer {
     @Override
     public void onInitialize() {
-        ServerLifecycleEvents.SERVER_STARTED.register(BluemapFrontiersFabricLike::init);
+        ServerLifecycleEvents.SERVER_STARTED.register(BluemapFrontiers::init);
         ServerLifecycleEvents.SERVER_STOPPING.register(BluemapFrontiers::stop);
     }
 }
